500de494af7723122e842a0af056aa46c171e0f9
RafaelBa <rafael.balski@gopopup.com>
2015-04-26 22:07:04 +0200

    Some heavy renaming and descriptions movement and added implementations for objects, Options, Eithers and Lists

--- subprojects/testing/src/test/scala/net/codealchemist/testing/specs/BuildInMatchersSpec.scala
+++ subprojects/testing/src/test/scala/net/codealchemist/testing/specs/BuildInMatchersSpec.scala
@@ -6,70 +6,79 @@ class BuildInMatchersSpec extends Specification { def is = "Specs2 Matchers".tit
 Specs2 comes with some ready to use matchers, and this here shows how to actually use them.
 
   There are some checks for equality like the equality of the
-    Object                       $e1
-    Type                         $e2
-    Reference                    $e3
+    Object                       $equality
+    Type                         $hasType
+    Reference                    $reference
 
   Matchers can help you with your Options, checking if it is
-    None                         $e4
-    Some                         $e5
-    You can even check if it is Some and matches a desired value  $e6
+    None                         $noneOpt
+    Some                         $someOpt
+    You can even check if it is Some and matches a desired value  $someVal
 
   There are Matchers that can handle Eithers, so you can check if your Either is
-    Left                         $e7
-    Right                        $e8
+    Left                         $leftEi
+    Right                        $rightEi
     It can even check if there is a matching value in your
-      Left                       $e9
-      Right                      $e10
-
-  Special Matchers for Lists can process them by looking if the List
-    contains some elements                                        $e11
-    contains some of these elements, but not neccessaryly all     $e12
-    contains exactly these elements, no more, no less             $e13
-    contains these elements in given order                        $e14
-    does not contain these elements                               $e15
-    contains elements matching the given predicates               $e16
-    contains elements of
-      given amount                                                $e17
-      amount in given range                                       $e18
+      Left                       $leftVal
+      Right                      $rightVal
 
   Matchers for Exceptions let you see if
-    an Exception of the right type is thrown                      $e19
+    an Exception of the right type is thrown                      $e11
     a Try returns a
-     Success with a specific value                                $e20
-     Failure with a specific value                                $e21
+     Success with a specific value                                $e12
+     Failure with a specific value                                $e13
 
   Map Matchers let you test for
-    Keys                                                          $e22
-    Values                                                        $e23
-    Pairs                                                         $e24
+    Keys                                                          $e14
+    Values                                                        $e15
+    Pairs                                                         $e16
 
   Some Matchers can be used on every Object, just like the beEqualTo, for example the
-    beLike                       $e25
-    beOneOf                      $e26
-    beNull                       $e27
-    haveClass                    $e28
-    beAnInstanceOf               $e29
+    beLike                       $e17
+    beOneOf                      $e18
+    beNull                       $e19
 
   Don't forget, that you can use Matchers inside other Matchers most of the time like in the
-    beSome                       $e30
-    beRight                      $e31
-    haveKeys                     $e32
-    haveValues                   $e33
-    exactly                      $e34
+    beSome                       $e20
+    beRight                      $e21
+    haveKeys                     $e22
+    haveValues                   $e23
+    exactly                      $e24
+    not                          $e25
+
+  Special Matchers for Lists can process them by looking if the List
+    has a specific size                                           ${L.size}
+    contains some elements                                        ${L.allOf}
+    contains exactly these elements, no more, no less             ${L.exact}
+    contains these elements in given order                        ${L.order}
+    does not contain these elements                               ${L.notContain}
+    contains elements matching the given predicates for
+      any element                                                 ${L.predicateAny}
+      all elements                                                ${L.predicateAll}
+    contains elements of
+      given amount                                                ${L.containAmount}
+      amount in given range                                       ${L.containRange}
+    contains all these elements, but distinguishing each          ${L.distinct}
 
 """
 
-  def e1 = failure
-  def e2 = failure
-  def e3 = failure
-  def e4 = failure
-  def e5 = failure
-  def e6 = failure
-  def e7 = failure
-  def e8 = failure
-  def e9 = failure
-  def e10 = failure
+  def equality = 42 should beEqualTo(42)
+  def hasType = "Instance" should beAnInstanceOf[String]
+  def reference = {
+    val s = "String"
+    val t = s
+    t should beTheSameAs(s)
+  }
+
+  def noneOpt = Option(null) should beNone
+  def someOpt = Option(12) should beSome
+  def someVal = Option("String") should beSome("String")
+
+  def leftEi = Left(42) should beLeft
+  def rightEi = Right("fourty-two") should beRight
+  def leftVal = Left(42) should beLeft(42)
+  def rightVal = Right("fourty-two") should beRight("fourty-two")
+
   def e11 = failure
   def e12 = failure
   def e13 = failure
@@ -85,13 +94,21 @@ Specs2 comes with some ready to use matchers, and this here shows how to actuall
   def e23 = failure
   def e24 = failure
   def e25 = failure
-  def e26 = failure
-  def e27 = failure
-  def e28 = failure
-  def e29 = failure
-  def e30 = failure
-  def e31 = failure
-  def e32 = failure
-  def e33 = failure
-  def e34 = failure
+
+  object L {
+    lazy val l = List(2, 3, 4)
+    
+    def size = l should have size(3)
+    def allOf = l should contain(4, 3)
+    def exact = List(1, 1, 2, 3, 4) should contain(exactly(1, 1, 4, 3, 2))
+    def order = l should contain(3, 4).inOrder
+    def notContain = l should not(contain(1, 5))
+    def predicateAny = l should contain(beGreaterThan(1), beGreaterThan(3))
+    def predicateAll = l should contain(beGreaterThan(1)).forall
+    def containAmount = l should contain(beLessThan(4)).exactly(2 times)
+    def containRange = l should contain(beLessThan(3)).between(0 times, 2 times)
+    def distinct = List(1, 1, 2, 3) should contain(1, 1, 2, 3).onDistinctValues
+  }
 }
+
+
