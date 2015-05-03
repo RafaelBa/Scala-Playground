6b214b8baa14802b9d0d7726e6864846a0cd918b
RafaelBa <rafael.balski@gopopup.com>
2015-05-03 22:34:08 +0200

    A short example of linking specs

---
+++ subprojects/testing/src/test/scala/net/codealchemist/testing/specs/subspec/SubOne.scala
@@ -0,0 +1,10 @@
+package net.codealchemist.testing.specs.subspec
+
+import org.specs2.Specification
+
+class SubOne extends Specification { def is = "SubSpec 1".title ^ s2"""
+  Getting the size of a list should result in its length  $e
+"""
+
+  def e = List(1, 2, 3).size should beEqualTo(3)
+}
